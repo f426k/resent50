97fe35bbffc8d3fe572a73b8c23cadb2a6c354e5
fuka-takashima <fuka.takashima@dena.com>
2023-07-20 10:19:45 +0900

    Initial Commit

---
+++ Resent50/Resent50App.swift
@@ -0,0 +1,17 @@
+//
+//  Resent50App.swift
+//  Resent50
+//
+//  Created by Fuka Takashima on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Resent50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
