11749d0502a2c20a1fa5e47ded4e374e2819a008
fuka-takashima <fuka.takashima@dena.com>
2023-07-20 12:24:12 +0900

    create

--- Resent50/ContentView.swift
+++ Resent50/ContentView.swift
@@ -6,16 +6,76 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
+
+    @State var classificationLabel = ""
+
+    func createClassificationRequest() -> VNCoreMLRequest {
+        do {
+            let configuration = MLModelConfiguration()
+
+            let model = try VNCoreMLModel(for: Resnet50(configuration: configuration).model)
+
+            let request = VNCoreMLRequest(model: model, completionHandler: { request, error in
+                performClassification(request: request)
+            })
+
+            return request
+
+        } catch {
+            fatalError("modelが読み込めません")
+        }
+    }
+
+    // 画像分類の処理
+    func performClassification(request:VNRequest){
+        guard let results = request.results else {
+            return
+        }
+
+        let classification = results as! [VNClassificationObservation]
+        classificationLabel = classification[0].identifier
+    }
+
+    // 実際に画像を分類する
+    func classifyImage(image:UIImage){
+        // 入力された画像の型をUIImageからCIImageに変換
+        guard let ciImage = CIImage(image:image) else {
+            fatalError("CIImageに変換できません")
+        }
+        // handler作る
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+
+        let classificationRequest = createClassificationRequest()
+
+        do{
+            try handler.perform([classificationRequest])
+        }catch{
+            fatalError("画像分類を失敗しました")
+        }
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(classificationLabel)
+                .padding()
+                .font(.title)
+
+            Image("cat")
+                .resizable()
+                .frame(width: 300,height: 200)
+
+            Button(action: {
+                classifyImage(image: UIImage(named: "cat")!)
+            }, label: {
+                Text("この画像は何の画像？")
+                    .padding()
+            })
         }
-        .padding()
+
     }
 }
 
